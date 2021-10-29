ace90df73f3225ee7e8ce1ffc1dffe525e3ffec6
Yun-SeYeong <dbstpdud09@naver.com>
2021-10-29 11:48:57 +0900

    [UPDATE] add gRPC Client

--- src/main/java/com/demo/GRPCClient.java
+++ src/main/java/com/demo/GRPCClient.java
@@ -2,9 +2,27 @@ package com.demo;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 
 public class GRPCClient {
+    private static HelloServiceGrpc.HelloServiceBlockingStub blockingStub;
+
     public static void main(String[] args) {
         ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8800").usePlaintext().build();
+
+        blockingStub = HelloServiceGrpc.newBlockingStub(channel);
+
+        HelloRequest request = HelloRequest.newBuilder()
+                .setFirstName("firstnameEx")
+                .setLastName("lastNameEx")
+                .build();
+
+        HelloResponse response;
+        try {
+            response = blockingStub.hello(request);
+        } catch (StatusRuntimeException e) {
+            return;
+        }
+        System.out.println("Greeting: " + response.getGreeting());
     }
 }
